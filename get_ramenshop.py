041adbedfa5dac80281379f96efd35e3bf99426e
ygkn <2000ygkn0713@gmail.com>
2019-12-16 14:59:52 +0900

    fix a bug of api

--- get_ramenshop.py
+++ get_ramenshop.py
@@ -1,9 +1,23 @@
 import urllib.request
+import urllib.parse
 import json
 
 apikey = "AIzaSyAOqwUYekkyQ4y3EgmXmhST4oMqckVmiVU"
+
+
 def get_ramenshop(location, radius):
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" +  apiKey + "&location=" + str(location.lat) + "," + str(location.lng) + "&radius=" + str(radius) + "&keyword=ラーメン屋"
+    url = (
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key="
+        + apikey
+        + "&location="
+        + str(location["lat"])
+        + ","
+        + str(location["lng"])
+        + "&radius="
+        + str(radius)
+        + "&keyword="
+        + urllib.parse.quote("ラーメン屋")
+    )
 
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
@@ -11,5 +25,7 @@ def get_ramenshop(location, radius):
 
         print(body)
 
-if __name__ == '__main__'
-    get_ramenshop({lat:34.9821826 ,lng:135.9610467}, 4000)
+
+if __name__ == "__main__":
+    get_ramenshop({"lat": 34.9821826, "lng": 135.9610467}, 4000)
+
